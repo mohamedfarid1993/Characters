d25003c51f165c899fd9e8a75337809fbd7bd0a3
mfaridghonaim <80623529+mfaridghonaim@users.noreply.github.com>
2024-05-30 17:54:16 +0300

    Initial Commit

---
+++ Characters/CharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharactersApp.swift
+//  Characters
+//
+//  Created by Mohamed Farid on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

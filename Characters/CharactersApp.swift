7f231b210e965278f702c7437f3f18c683197578
mfaridghonaim <80623529+mfaridghonaim@users.noreply.github.com>
2024-05-30 22:15:45 +0300

    Add uikit view as an initial view

--- Characters/CharactersApp.swift
+++ Characters/CharactersApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CharactersApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CharactersListViewControllerRepresentable()
         }
     }
 }
---
+++ Characters/View/Characters List/CharactersListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CharactersListViewController.swift
+//  Characters
+//
+//  Created by Mohamed Farid on 30/05/2024.
+//
+
+import UIKit
+
+class CharactersListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Characters"
+    }
+}

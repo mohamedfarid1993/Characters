5fb819bb5df0c973e68a079f1434bc3d33c259b9
mfaridghonaim <80623529+mfaridghonaim@users.noreply.github.com>
2024-05-31 15:44:32 +0300

    Add naivgation controller appearence setup

---
+++ Characters/Helpers/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  Characters
+//
+//  Created by Mohamed Farid on 31/05/2024.
+//
+
+import UIKit
+
+enum Theme {
+    
+    enum Colors {
+        static let titlesDarkPurple = UIColor(red: 23/255, green: 1/255, blue: 69/255, alpha: 1)
+    }
+}
--- Characters/View/Characters List/CharactersListViewController.swift
+++ Characters/View/Characters List/CharactersListViewController.swift
@@ -8,10 +8,41 @@
 import UIKit
 
 class CharactersListViewController: UIViewController {
-
+    
+    // MARK: Properties
+    
+    private let viewModel = CharactersListViewModel()
+    
+    // MARK: - Life Cycle View Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.setupNavigationBar()
+    }
+}
 
+// MARK: - Setup Navigation Appearance
+
+extension CharactersListViewController {
+    private func setupNavigationBar() {
         self.title = "Characters"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let standardAppearance = UINavigationBarAppearance()
+        standardAppearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: Theme.Colors.titlesDarkPurple]
+        standardAppearance.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: Theme.Colors.titlesDarkPurple]
+        standardAppearance.configureWithOpaqueBackground()
+        standardAppearance.backgroundColor = UIColor.clear
+        standardAppearance.shadowImage = UIImage()
+        standardAppearance.shadowColor = nil
+        
+        self.navigationController?.navigationBar.standardAppearance = standardAppearance
+        self.navigationController?.navigationBar.scrollEdgeAppearance = standardAppearance
     }
 }

666c82b4479c11a6f0f5a669fe400c45e0ed630e
mfaridghonaim <80623529+mfaridghonaim@users.noreply.github.com>
2024-05-31 18:47:53 +0300

    Add theme colors

--- Characters/Helpers/Theme.swift
+++ Characters/Helpers/Theme.swift
@@ -12,5 +12,8 @@ enum Theme {
     enum Colors {
         static let titlesDarkPurple = UIColor(red: 23/255, green: 1/255, blue: 69/255, alpha: 1)
         static let subtitlesLightPurple = UIColor(red: 110/255, green: 102/255, blue: 142/255, alpha: 1)
+        static let borderLightPurple = UIColor(red: 244/255, green: 242/255, blue: 246/255, alpha: 1)
+        static let femaleLightPink = UIColor(red: 255/255, green: 231/255, blue: 236/255, alpha: 1)
+        static let maleLightBlue = UIColor(red: 234/255, green: 246/255, blue: 252/255, alpha: 1)
     }
 }
